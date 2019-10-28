7f6430cc726e77c8fcb9f183c3e4f1e318dacd1b
joshuaedon <joshuaedon7@gmail.com>
2019-10-28 23:02:44 +0000

    Sea level created

--- Evolution Simulation/Assets/GridController.cs
+++ Evolution Simulation/Assets/GridController.cs
@@ -3,7 +3,9 @@
 public class GridController : MonoBehaviour {
     public static int cols = 50;
     public static int rows = 30;
-    public float scale = 0.01f;
+    public float scale = 5f;
+    public float seaLevel = 0.4f;
+    public int seaBorder = 3;
     GameObject[,] gridArray;
 
     void Start() {
@@ -13,17 +15,21 @@ public class GridController : MonoBehaviour {
         for(int r = 0; r < rows; r++) {
             for(int c = 0; c < cols; c++) {
                 GameObject chunk = (GameObject)Instantiate(referenceChunk, transform);
-                chunk.transform.position = new Vector3(c, 0, r);
 
-                float elevation = Mathf.PerlinNoise(c * 1f / cols, r * 1f / rows);
+                float elevation = Mathf.PerlinNoise((float)c / cols * scale, (float)r / rows * scale);
+                chunk.transform.position = new Vector3(c, elevation / 2, r);
                 chunk.transform.localScale = new Vector3(1f, elevation, 1f);
 
-                float col = 0.3f + 0.7f * ((elevation - 0.5f) / (1f - 0.5f));
+                float col = Mathf.Clamp(0.3f + 0.7f * ((elevation - seaLevel) / (1f - seaLevel)), 0, 1);
                 chunk.GetComponent<Renderer>().material.color = new Color(1f - col, 1f - col * 0.6f, 1f - col);
                 gridArray[c, r] = chunk;
             }
         }
         Destroy(referenceChunk);
+
+        transform.Find("Water").transform.position = new Vector3(0, seaLevel, 0);
+        float seaFloorCol = Mathf.Clamp(0.3f - 0.7f * seaLevel / (1f - seaLevel), 0, 1);
+        transform.Find("Sea Floor").GetComponent<Renderer>().material.color = new Color(1f - seaFloorCol, 1f - seaFloorCol * 0.6f, 1f - seaFloorCol);
 
         transform.position = new Vector3(-(cols - 1) / 2, 0, -(rows - 1) / 2);
     }
