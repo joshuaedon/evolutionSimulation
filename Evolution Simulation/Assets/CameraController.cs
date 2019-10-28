36ff410f2f80e37ad906a92739663b7dda7a7fb8
joshuaedon <joshuaedon7@gmail.com>
2019-10-28 19:35:18 +0000

    Basic grid and camera movement

---
+++ Evolution Simulation/Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public float panLimit = 10f;
+
+    public float scrollSpeed = 20f;
+    public float minY = 10f;
+    public float maxY = 120f;
+
+    void Update() {
+        Vector3 pos = transform.position;
+        
+        // Time.deltaTime = time ellapsed since last frame
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+            pos.z += panSpeed * Time.deltaTime;
+        if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            pos.z -= panSpeed * Time.deltaTime;
+        if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+            pos.x += panSpeed * Time.deltaTime;
+        if(Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            pos.x -= panSpeed * Time.deltaTime;
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        //pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+        pos += Vector3.Normalize(Camera.main.transform.forward) * scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -(GridManager.cols/2 + panLimit), GridManager.cols/2 + panLimit);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -(GridManager.rows/2 + panLimit), GridManager.rows/2 + panLimit);
+        transform.position = pos;
+    }
+}
---
+++ Evolution Simulation/Assets/GridManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+    public static int cols = 50;
+    public static int rows = 30;
+    public float scale = 0.01f;
+    GameObject[,] gridArray;
+
+    void Start() {
+        GameObject referenceChunk = (GameObject)Instantiate(Resources.Load("Chunk"));
+        gridArray = new GameObject[cols, rows];
+
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                GameObject chunk = (GameObject)Instantiate(referenceChunk, transform);
+                chunk.transform.position = new Vector3(c, 0, r);
+                //float elevation = Mathf.PerlinNoise(c*scale, r*scale);
+                //chunk.transform.localScale = new Vector3(1f, elevation, 1f);
+                /*if(elevation >= 0.5)
+                    chunk.GetComponent<MeshRenderer>().material = ;*/
+                gridArray[c, r] = chunk;
+            }
+        }
+        Destroy(referenceChunk);
+
+        transform.position = new Vector3(-(cols-1)/2, 0, -(rows-1)/2);
+        transform.eulerAngles -= new Vector3(10, 10, 0);
+        transform.position += new Vector3(0, 0, 32);
+    }
+}
